ed5ef8cc6aa16c3765e554fa0505b5bf2ada4bf4
Nelson Wasiuk <drpablovyt@gmail.com>
2026-08-03 17:05:39 -0300

    Aumentar limite de upload pra 10mb

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
       { protocol: "https", hostname: "*.supabase.co" },
     ],
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
